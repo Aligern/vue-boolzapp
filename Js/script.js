efe1f64c79d07f57694f561400e2fca663793c5b
Aligern <sasso.luca93@gmail.com>
2024-04-04 12:00:01 +0200

    added milestone 3 and 4

--- Js/script.js
+++ Js/script.js
@@ -6,19 +6,39 @@ createApp ({
     data() {
         return {
             contacts: contacts,
-            activeContactId: 1
+            activeContactId: 1,
+            msgText: '',
+            searchText: '',
         }
     },
     methods:{
         selectContact(id) {
             this.activeContactId = id;
-        }
+        },
+        sendMessage() {
+            const newMsg = {
+                date: new Date().toLocaleString(),
+                message: this.msgText,
+                status: 'sent'
+            }
+            this.activeContact.messages.push(newMsg);
+            this.msgText = '';
+            setTimeout(() => {const newMsg = {
+                date: new Date().toLocaleString(),
+                message: 'ok',
+                status: 'received'
+            }
+            this.activeContact.messages.push(newMsg);
+            },1000);
+        },
     },
     computed:{
         activeContact() {
             return this.contacts.find((contact) => contact.id === this.activeContactId)
+        },
+        filteredContacts() {
+            return this.contacts.filter((el) => el.name.toLowerCase().includes(this.searchText.toLowerCase()));
         }
-
     },
     mounted() {
         console.log(this.contacts);
